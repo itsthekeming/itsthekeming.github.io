9353388c440094f7be0872f56aa7a65217cfefef
Edward Sanders <esanders@hey.com>
2021-02-24 19:36:00 -0500

    Added Typescript, added TailwindCSS, edited homepage and favicon, removed unneeded files

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/globals.css";
+import "@fontsource/inter";
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Edward Sanders</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen w-screen space-y-8 bg-gray-800 text-gray-200 text-center">
+      <h1 className="text-9xl">Hello</h1>
+      <p className="text-4xl">I'm Edward</p>
+      <p className="text-4xl">This is my personal website</p>
+      <p className="text-4xl">I'll be building it out over the coming months</p>
+      <p className="text-4xl">For now, here's where you can find me:</p>
+      <span className="flex flex-row space-x-8 text-2xl underline active:text-gray-50">
+        <a href="https://twitter.com/itsthekeming">Twitter</a>
+        <a href="https://github.com/itsthekeming">GitHub</a>
+        <a href="https://www.linkedin.com/in/esanders1994/">LinkedIn</a>
+      </span>
+    </div>
+  );
+}
