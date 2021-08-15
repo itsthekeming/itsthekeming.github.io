58c269c388797ef79e395e5eff7b26f53dc8e2ea
Edward Sanders <edward.sanders@dcsg.com>
2021-08-14 20:30:49 -0400

    Switched to yarn, rearranged project, updated deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  purge: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: {
     extend: {
       fontFamily: { sans: ["Inter var", ...defaultTheme.fontFamily.sans] },
     },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 };
