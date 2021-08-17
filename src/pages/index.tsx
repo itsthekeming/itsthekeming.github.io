025de5a7d546a143cfd1f732ec078346dd639f32
Edward Sanders <esanders@hey.com>
2021-08-16 20:23:02 -0400

    Fix minor style issues on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen bg-gradient-to-tr from-[#00F260] to-[#0575E6] text-white/95">
-      <div className="flex flex-col items-center border-white/90 border-4 p-12 rounded-md">
-        <h1 className="text-6xl mb-12">Edward Sanders</h1>
+      <div className="flex flex-col items-center border-white/90 border-4 p-12 w-11/12 sm:w-auto rounded-md">
+        <h1 className="text-center text-6xl mb-12">Edward Sanders</h1>
         <Link href="/resume" passHref>
           <a className="underline text-3xl">Résumé</a>
         </Link>
